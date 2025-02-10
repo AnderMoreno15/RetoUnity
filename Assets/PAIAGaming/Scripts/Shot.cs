bc25669de64dc57654d76061b0c20a2fdc77c416
InigoFreire <102539351+InigoFreire@users.noreply.github.com>
2025-02-10 12:19:51 +0100

    Added sound effecto for bullet

--- Assets/PAIAGaming/Scripts/Shot.cs
+++ Assets/PAIAGaming/Scripts/Shot.cs
@@ -8,16 +8,23 @@ public class Shot : MonoBehaviour
     public float shootRate = 0.2f;
 
     private float shootRateTime = 0f;
+    AudioSource shotAudioClip;
+
+    void Start()
+    {
+        shotAudioClip = GetComponent<AudioSource>(); // Asegúrate de asignar el componente AudioSource
+    }
 
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
+            shotAudioClip.Play(); // Corregido el error aquí
             if (Time.time > shootRateTime)
             {
                 GameObject newBullet; // Crear bala nueva
                 newBullet = Instantiate(bullet, spawnPoint.position, spawnPoint.rotation); // Instanciar la bala
-                newBullet.GetComponent<Rigidbody>().AddForce(spawnPoint.forward * shootForce); // Aplicar fuerza a la bala
+                newBullet.GetComponent<Rigidbody>().AddForce(spawnPoint.forward * shootForce); // Aplicar spawnpoint y fuerza a la bala
                 shootRateTime = Time.time + shootRate; // Actualizar el tiempo de disparo
                 Destroy(newBullet, 2f); // Destruir la bala en 2 segundos
             }
